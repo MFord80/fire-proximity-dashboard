0517b69295c27fa18dd3b89bf8b6e8b59c4c0189
MFord80 <matthewfford@gmail.com>
2022-10-27 09:13:13 +0800

    Basic map completed with layers

---
+++ app.py
@@ -0,0 +1,40 @@
+# Import Flask
+from flask import Flask, render_template
+import requests
+import json
+
+# Create app, pass __name__
+app = Flask(__name__)
+
+# Define index route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Define /hotspots route
+@app.route("/hotspots")
+def hotspots():
+    return render_template("hotspots.json")
+
+# Define /schools route
+@app.route("/schools")
+def schools():
+    return render_template("Current_Active_Schools_Sem_1_2022_Public_DET_020_WA_GDA2020_Public.geojson")
+
+# Define /hospitals route
+@app.route("/hospitals")
+def hospitals():
+    return render_template("Health_Hospitals_HEALTH_001_WA_GDA2020_Public.geojson")
+
+# Define /agedcare route
+@app.route("/agedcare")
+def agedcare():
+    return render_template("WA-30-June-2022.csv")
+
+# Define /incidents route, run scrape application
+# @app.route("/incidents")
+# def scraper():
+
+# Run
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/fire-proximity.js
@@ -0,0 +1,130 @@
+// OSM tile layer
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+// Toner lite tile layer
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+});
+
+// Topographic tile layer
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// Icon variables
+var schoolIcon = L.icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+  iconAnchor: [12,40]
+});
+var hospitalIcon = L.icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png",
+  iconAnchor: [24,81]
+});
+var agedCareIcon = L.icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+  iconAnchor: [12,40]
+});
+
+// Nested Overlay layers
+// School layer
+$.getJSON("/schools", (function(response) {
+  features = response.features;
+  const schoolMarkers =[]
+  for (let i = 0; i < features.length; i++) {
+    let location = features[i].geometry;
+    if(location){
+      schoolMarkers.push(L.marker([location.coordinates[1], location.coordinates[0]], {icon: schoolIcon}));
+    }
+  }
+  // Hospital layer
+  $.getJSON("/hospitals", (function(response) {
+    features = response.features;
+    const hospitalMarkers =[]
+    for (let i = 0; i < features.length; i++) {
+      let location = features[i].geometry;
+      if(location){
+        hospitalMarkers.push(L.marker([location.coordinates[1], location.coordinates[0]], {icon: hospitalIcon}));
+      }
+    }
+    // Aged Care Facility layer
+    d3.csv("/agedcare").then(function(response) {
+      features = response
+      const agedCareMarkers =[]
+      for (let i = 0; i < features.length; i++) {
+        let location = features[i];
+        if(location){
+          agedCareMarkers.push(L.marker([location.Latitude, location.Longitude], {icon: agedCareIcon}));
+        }
+      }
+      // Fire hotspots layer
+      $.getJSON("/hotspots", (function(response) {
+        features = response
+        hotspotCircles = []
+        for (let i = 0; i < Object.keys(features).length; i++) {
+          if (features[String(i)].temperature < 330) {
+            heat = "#ffcc00"
+          } else if (features[String(i)].temperature < 360) {
+            heat = "#e65c00"
+          } else {
+            heat = "#cc0000"
+          };
+          let location = features[String(i)].coordinates;
+          if(location){
+            hotspotCircles.push(L.circle([location[0], location[1]], {
+              color: heat,
+              opacity: 0,
+              fillColor: heat,
+              fillOpacity: 0.5,
+              radius: 200 
+            }))
+          }
+        }
+      // Layer group variables
+      let hotspots = L.layerGroup(hotspotCircles)
+      let schools = L.layerGroup(schoolMarkers)
+      let hospitals = L.layerGroup(hospitalMarkers)
+      let agedCare = L.layerGroup(agedCareMarkers)
+
+      // Layer variables
+      var baseMap = {
+        "Streetmap": osm,
+        "Toner": Stamen_TonerLite,
+        "Topographic": OpenTopoMap
+      };
+        
+      var overlayMaps = {
+        "Hotspots": hotspots,
+        "Schools": schools,
+        "Hospitals": hospitals,
+        "Aged Care Facilities": agedCare
+      };
+      
+      // Map variable
+      var Map = L.map("map", {
+        center: [-31.95, 116.1],
+        zoom: 13,
+        layers: [osm, hotspots]
+      });
+      
+      // Layer control
+      L.control.layers(baseMap, overlayMaps, {
+        collapsed: false
+      }).addTo(Map);  
+      }));
+    });
+  }));
+}));
+
+
+
+
+
+
+
